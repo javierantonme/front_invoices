ca9ab932d12ce7d671e5449abcdeae29d5362586
Javier <javierantonme@gmail.com>
2024-11-19 22:49:24 +1100

    Ajuste variable de entorno

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // Cambia esta URL por la de tu backend
+  baseURL: import.meta.env.VITE_API_URL, // Cambia esta URL por la de tu backend
 });
 
 // Interceptor para agregar el token en las solicitudes
